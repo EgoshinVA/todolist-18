aba8c1dddb069e71dc42b44fb185d722f75f0c12
EgoshinVA <egoshinvit@gmail.com>
2025-02-04 22:31:29 +0700

    todolist 18

---
+++ src/app/Main.tsx
@@ -0,0 +1,31 @@
+import Container from "@mui/material/Container"
+import Grid from "@mui/material/Unstable_Grid2"
+import { Path } from "common/router"
+import { AddItemForm } from "common/components"
+import { useAppDispatch, useAppSelector } from "common/hooks"
+import { Navigate } from "react-router-dom"
+import { selectIsLoggedIn } from "../features/auth/model/authSlice"
+import { addTodolistTC } from "../features/todolists/model/todolistsSlice"
+import { Todolists } from "../features/todolists/ui/Todolists/Todolists"
+import { useAddTodolistMutation } from "../features/todolists/api/todolistsApi"
+
+export const Main = () => {
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
+
+  const [addTodolist] = useAddTodolistMutation()
+
+  if (!isLoggedIn) {
+    return <Navigate to={Path.Login} />
+  }
+
+  return (
+    <Container fixed>
+      <Grid container sx={{ mb: "30px" }}>
+        <AddItemForm addItem={addTodolist} />
+      </Grid>
+      <Grid container spacing={4}>
+        <Todolists />
+      </Grid>
+    </Container>
+  )
+}
---
+++ src/app/store.ts
@@ -0,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit"
+import { authReducer, authSlice } from "../features/auth/model/authSlice"
+import { tasksReducer, tasksSlice } from "../features/todolists/model/tasksSlice"
+import { todolistsReducer, todolistsSlice } from "../features/todolists/model/todolistsSlice"
+import { appReducer, appSlice } from "./appSlice"
+import { setupListeners } from "@reduxjs/toolkit/query"
+import { baseApi } from "./baseApp"
+
+export const store = configureStore({
+  reducer: {
+    [tasksSlice.name]: tasksReducer,
+    [todolistsSlice.name]: todolistsReducer,
+    [appSlice.name]: appReducer,
+    [authSlice.name]: authReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
+})
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
+
+setupListeners(store.dispatch)
